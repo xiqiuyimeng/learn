5c97fc773e8d5e9ee0c5111544bb577b71eebf21
xiqiuyimeng <847466569@qq.com>
2020-06-01 11:24:11 +0800

    全局异常可以指定要被管理的类

--- src/main/java/com/demo/learn/exception/GlobalExceptionHandler.java
+++ src/main/java/com/demo/learn/exception/GlobalExceptionHandler.java
@@ -3,8 +3,9 @@ package com.demo.learn.exception;
 import com.demo.learn.result.ResultEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -12,10 +13,17 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * 全局异常处理器，优先捕获子类异常
+ * 指定处理的包：@RestControllerAdvice(basePackages = {"com.demo.learn.controller"})
+ *              @RestControllerAdvice("com.demo.learn.controller")
+ *              @RestControllerAdvice(value = "com.demo.learn.controller")
+ * 指定具体的controller类：@RestControllerAdvice(basePackageClasses = {TestController.class})
+ *                       @RestControllerAdvice(assignableTypes = {TestController.class})
+ * 指定具体的注解，被这些注解标记的controller会被管理：@RestControllerAdvice(annotations = {RestController.class})
  * @author luwt
  * @date 2020/5/13.
  */
 @RestControllerAdvice
+@ResponseStatus(HttpStatus.OK)
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
@@ -26,9 +34,9 @@ public class GlobalExceptionHandler {
      * @return
      */
     @ExceptionHandler(value = BusinessException.class)
-    public ResponseEntity<?> businessExceptionHandler(BusinessException be) {
+    public ResultEntity businessExceptionHandler(BusinessException be) {
         logger.error("业务异常，异常类型为：{}，异常信息：{}，堆栈信息：", be.getClass(), be.getMessage(), be);
-        return ResponseEntity.ok().body(new ResultEntity<>(be.getException()));
+        return new ResultEntity<>(be.getException());
     }
 
     /**
@@ -37,7 +45,7 @@ public class GlobalExceptionHandler {
      * @return
      */
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<?> exceptionHandler(HttpServletRequest request, Exception e) {
+    public ResultEntity exceptionHandler(HttpServletRequest request, Exception e) {
         ResultEntity resultEntity;
         if (e instanceof NoHandlerFoundException) {
             resultEntity = new ResultEntity(ExceptionEnum.HANDLER_NOT_FOUND);
@@ -45,6 +53,6 @@ public class GlobalExceptionHandler {
             resultEntity = new ResultEntity(ExceptionEnum.UNDEFINED);
         }
         logger.error("未知异常，异常类型为：{}，异常信息：{}，堆栈信息：", e.getClass(), e.getMessage(), e);
-        return ResponseEntity.ok().body(resultEntity);
+        return resultEntity;
     }
 }
