7c47d7143d24109af185e297b7f2e57a86786b93
xiqiuyimeng <847466569@qq.com>
2020-12-17 16:18:45 +0800

    获取ip脚本

--- get_ip.sh
+++ get_ip.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 # 获取ip地址
 function getIP() {
-    NETWORK_CARD_NAME=`ls /etc/sysconfig/network-scripts | grep 'ifcfg-' | grep -v 'ifcfg-lo' | cut -d "-" -f2`
-    IP_INFO_NUM=$((`ifconfig | sed "/^$NETWORK_CARD_NAME/=" -n` + 1))
-    ifconfig | sed "$IP_INFO_NUM p" -n | awk '{print $2}'
+    # /sys/class/net 目录下存储了所有网卡信息，子目录均为网卡名，
+    # /sys/devices/virtual/net/ 目录下存储了所有的虚拟网卡名
+    NETWORK_CARD_NAME=`ls /sys/class/net | grep -v "\`ls /sys/devices/virtual/net/\`"`
+    for net_name in $NETWORK_CARD_NAME
+    do
+        possible_ip=`ip a | sed "/$net_name$/p" -n | awk '{print $2}'| awk -F/ '{print$1}'`
+        if [ $possible_ip ]; then
+            echo $possible_ip
+        fi
+    done
 }
-echo `getIP`
+
+function getIPStr() {
+    ix=1
+    for ip in `getIP`
+    do
+        let ix+=1
+        echo "IP.$ix = $ip\n"
+    done
+}
+
+# 打印
+IP_STR=$(echo `getIPStr` | sed 's/\\n\s/\\n/g')
+HOSTNAME_STR="DNS.2 = `hostname`"
+echo -e "本机ip为：\n`getIP`\nhostname为：`hostname`"
