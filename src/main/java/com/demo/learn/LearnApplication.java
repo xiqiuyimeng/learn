c342ce9a13fbe9a66a032f9c7a1735735e7bebe4
xiqiuyimeng <847466569@qq.com>
2021-09-28 18:39:13 +0800

    当读取apollo配置，和代码中不符合时，可以采用beanFactoryPostProcessor的形式来调整配置项

--- src/main/java/com/demo/learn/LearnApplication.java
+++ src/main/java/com/demo/learn/LearnApplication.java
@@ -1,11 +1,22 @@
 package com.demo.learn;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableApolloConfig
 @SpringBootApplication
 public class LearnApplication {
 
+    /**
+     * 为了支撑使用apollo配置，需要在启动时，添加vm参数如下：
+     * -Denv=local
+     * -Dapollo.cacheDir=src/main/resources/apollo，指定为apollo配置文件所在的目录（当前项目已经配置，按此配置即可），
+     *      apollo会自动在此目录下搜寻配置文件：
+     *          learn/config-cache/learn+default+application.properties，
+ *          app.id在META-INF/app.properties文件中定义
+     * @param args args
+     */
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
     }
---
+++ src/main/java/com/demo/learn/util/PropertySplitRunner.java
@@ -0,0 +1,31 @@
+package com.demo.learn.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 为了展示 {@link com.demo.learn.postProcessor.PropertySourceSplitProcessor} 拦截拆分属性效果而创建
+ * @author luwt
+ * @date 2021/9/28
+ */
+@Slf4j
+@Component
+public class PropertySplitRunner implements CommandLineRunner {
+
+    @Value("${test.url}")
+    private String url;
+
+    @Value("${test.host}")
+    private String host;
+
+    @Value("${test.api}")
+    private String api;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("获取到的test.url为:{}, test.host为:{}, test.api为:{}", url, host, api);
+    }
+
+}
