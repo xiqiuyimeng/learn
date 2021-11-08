6dd04248f109aeda7da693a122ac94277b91a8d4
xiqiuyimeng <847466569@qq.com>
2021-11-08 14:22:48 +0800

    @ControllerAdvice 配合 RequestBodyAdvice 和 ResponseBodyAdvice 实现打印日志或者加解密数据

---
+++ src/main/java/org/demo/learn/logAdvice/ResponseLogAdvice.java
@@ -0,0 +1,34 @@
+package org.demo.learn.logAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author luwt
+ * @date 2021/11/8
+ */
+@Slf4j
+@ControllerAdvice
+public class ResponseLogAdvice implements ResponseBodyAdvice {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+        log.info("使用ResponseLogAdvice打印的responseBody is {}", body);
+        return body;
+    }
+}
