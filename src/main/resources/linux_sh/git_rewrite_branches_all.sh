dc922b209682409f97ed60b863317032bcf74ca4
xiqiuyimeng <847466569@qq.com>
2022-04-27 10:59:18 +0800

    git分支提交者修改脚本，将所有不是指定提交者的信息刷为指定提交者

---
+++ src/main/resources/linux_sh/git_rewrite_branches_all.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# 将历史的提交，修改提交人和作者信息
+
+git filter-branch --env-filter '
+
+
+# 新的邮箱以及用户名
+CORRECT_NAME="xiqiuyimeng"
+CORRECT_EMAIL="847466569@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_COMMITTER_NAME" != "$CORRECT_NAME" ]
+then 
+	export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" != "$CORRECT_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch Rakefile' HEAD
